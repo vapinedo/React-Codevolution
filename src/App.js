202e691065258a7515709c159689c0a74efd20ea
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-05-05 07:40:13 -0500

    [ADD] Methods as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <ParentComponent />
     </div>
   );
 }
