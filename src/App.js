9053a7a3b4bdde997378cb321a6e420fd01ff6b9
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-05-09 18:05:24 -0500

    [ADD] Basics of Form Handling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import NameList from './components/NameList';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <Form />
     </div>
   );
 }
