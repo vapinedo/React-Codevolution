2a076bf0c2e85416eff23f957c631b859a820756
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-05-17 16:19:23 -0500

    [ADD] Forwarding Refs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput />
+      <FRParentInput />
     </div>
   );
 }
